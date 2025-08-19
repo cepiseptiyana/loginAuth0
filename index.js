d8e031cbe598543f5c0ea2273599c55f40063528
cepiseptiyana <cepifams3@gmail.com>
2025-08-20 00:07:25 +0700

    feat finish

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ app.use(
     saveUninitialized: true,
     cookie: {
       httpOnly: true,
-      secure: false, // wajib true di production (https)
-      sameSite: "none", // wajib kalau frontend beda domain
+      secure: true, // wajib true di production (https)
+      // sameSite: "none", // wajib kalau frontend beda domain
     },
   })
 );
