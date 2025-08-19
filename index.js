568b4dbd201c0517ca556034b801ac7402077a4f
cepiseptiyana <cepifams3@gmail.com>
2025-08-19 23:20:07 +0700

    feat finish

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://shoping-ruddy.vercel.app/",
+    origin: "https://shoping-ruddy.vercel.app",
     // origin: "http://localhost:5173", // frontend dev URL
     credentials: true, // penting untuk cookies/session
   })
